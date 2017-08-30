351eedc6167d117fbcd9cceb600eb251177a5dfb
alcofrisbas <bgreene231@gmail.com>
2017-08-29 17:56:18 -0700

    more soundOp changes

--- scripts/soundOps.py
+++ scripts/soundOps.py
@@ -1,6 +1,7 @@
 import numpy as np
 import soundfile as sf
 import time
+import math
 
 from scipy.signal import get_window
 
@@ -45,17 +46,17 @@ def framesPerSecond2(length, numFrames):
 	return numFrames/length 
 
 def infoAt(hPhase, f):
-	return hPhase[f, 0]
+	return hPhase[f, 0]									#returns information at that point
 def pitchRatio(consF, vowelF):
 	if consF == 0 or vowelF == 0:
 		return 1
-	return consF/vowelF
+	return consF/vowelF 								#returns the pitch ratio
 
 
-def retune(fs, y, stepRatio):
+def retune(fs, y, stepRatio):							#retunes the given sound using linear interpolation
     
     lastI = len(y)/float(stepRatio)
-    print lastI
+    #print lastI
     newsound = []
     for i in range(int(lastI+1)):
         newsound.append(0)
@@ -104,14 +105,29 @@ def stablePoint(inHarmonicArray, surveyDepth, threshold, stabilityValue, pad):
 			break
 	return cutPoint+pad
 
-def f0phaseShiftAmount(consPhase, vowelPhase, consFS, consFreqF0):
-	samplesPerWave = float(consFS)/consFreqF0			#aka period, don't know why i didnt call it that
+def adjustPhase(H, consFS, conshfreq, conshphase, cfLF, swF, vowelhphase):
+	HopTime = float(H)/consFS
+	freqPeriod = 1/conshfreq[swF, 1]
 
-def crossFadePitch(conshfreq, vowelhfreq, swF, cfL):
-	pass#for 
+	for i in range(conshphase.shape[1]):
+		for j in range(1, cfLF):
+			before = conshphase[swF-cfLF+j+1, i]
+			conshphase[swF-cfLF+j+1, i] = (conshphase[swF-cfLF+j, i] + (2*math.pi)*HopTime*conshfreq[swF-cfLF+j, i]) % (2*math.pi)
+		phaseCorr = (vowelhphase[swF, i] - conshphase[swF, i])% (2*math.pi)
+		phaseCorrIncremental = -1*phaseCorr/cfLF
+		for j in range(1, int(cfLF+1)):
+			conshphase[swF-cfLF+j, i] = (conshphase[swF-cfLF+j, i]+phaseCorrIncremental)%(2*math.pi)
+
+def crossFadePitch(conshfreq, vowelhfreq, swF, cfLF):
+	start = swF-cfLF
+	for i in range(1, conshfreq.shape[1]):
+		if conshfreq[swF - cfLF, i] == 0:
+			continue
+		PitchDiff = conshfreq[swF, i]-conshfreq[swF - cfLF, i]
+		for j in range(1, int(cfLF)):
+			conshfreq[start+j, i] = conshfreq[start+j, i]+((j/cfLF)*PitchDiff)
 
 def adjustMagnitude(conshmag, vowelhmag, stable, vowelPadF):
-	print conshmag.shape
 	for i in range(conshmag.shape[1]):
 
 		ratio = conshmag[stable, i]/vowelhmag[stable+vowelPadF, i]
@@ -125,7 +141,6 @@ def simpleXFade(consX, vowelX, consFS, swF, cfL, conshfreq):
 		consX = np.concatenate((consX, np.zeros(addAmount)))
 	else:
 		consX = np.resize(consX, vowelX.shape[0])
-	print int(swSample-cfL), consX.shape[0]
 	
 	for i in range(0, consX.shape[0]):
 		if i < swSample-cfL:
@@ -139,89 +154,85 @@ def simpleXFade(consX, vowelX, consFS, swF, cfL, conshfreq):
 		else:
 			consX[i] = 0
 
-	sf.write("output_sounds/simpleXFadeCons.wav", consX, consFS)
-	sf.write("output_sounds/simpleXFadeVowel.wav", vowelX, consFS)
+	#sf.write("output_sounds/simpleXFadeCons.wav", consX, consFS)
+	#sf.write("output_sounds/simpleXFadeVowel.wav", vowelX, consFS)
 
 	x = np.add(consX, vowelX)
-	sf.write("output_sounds/simpleXFade.wav", x, consFS, subtype="PCM_24")
-	#sf.write("output_sounds/simpleXFade.wav", )
+	#sf.write("output_sounds/simpleXFade.wav", x, consFS, subtype="PCM_24")
+	return x
+
 def makeSound(hfreq, hmag, hphase, xr, Ns, H, fs):
 	y, yh = HPR.hprModelSynth(hfreq, hmag, hphase, xr, Ns, H, fs)
 	return y, yh
 
-def main():
-	startTime = time.time()
-	consFile = "../Morgan_44.1/Mod/Mod_18.wav"
-	vowelFile = "../Morgan_44.1/Ah Main/Ah Main_20.wav"
+def writeSound(fileName, x, fs):
+	sf.write(fileName, x, fs, subtype="PCM_24")
 
-	consX, consFS = soundToArray(consFile)
-	#print consX, consFS
-	vowelX, vowelFS = soundToArray(vowelFile)
-	#print vowelX, vowelFS
-	if consFS != vowelFS:
+def splice(consFile, vowelFile, cfL, pad, vowelPadMs, f0min, f0max, Ns=512, H=128):
+	consX, consFS = soundToArray(consFile)				#read both files to arrays
+	vowelX, vowelFS = soundToArray(vowelFile)			#and capture their sample rate
+
+	if consFS != vowelFS:								#make sure that sample rates match
 		raise ValueError("Please use samples with matching Sample Rates")
-	vowelPadMs = 700
-	vowelX, vowelFS = zeroPad(vowelX, vowelFS, 700)		#add .7 seconds to vowel
 
-	conshfreq, conshmag, conshphase, consxr = fourierResidual(consX, consFS)
-														#construct harmonic arrays from consonant sound
-	vowelhfreq, vowelhmag, vowelhphase, vowelxr = fourierResidual(vowelX, vowelFS)
-														#construct harmonic arrays from padded vowel sound
+	vowelX, vowelFS = zeroPad(vowelX, vowelFS, 700)		#rewrite the vowel file with
+														#a pad of VOWELPADMS ms
+
+	conshfreq, conshmag, conshphase, consxr = fourierResidual(consX, consFS, minf0 = f0min, maxf0 = f0max)
+	vowelhfreq, vowelhmag, vowelhphase, vowelxr = fourierResidual(vowelX, vowelFS, minf0 = f0min, maxf0 = f0max)
+														#using SMS Tools, construct arrays
+														#of the components of each sound
 	vowelPadF = (vowelPadMs/1000.0)*framesPerSecond2(soundLength(vowelX, vowelFS), numFrames(vowelhfreq))
+														#the vowel pad in Frames(vs samples or ms)
+	cfLF = cfL/samplesPerFrame(consX, conshfreq)		#cfL in Frames(vs samples)
+	
+	freqStable = stablePoint(conshfreq, 0, 100, 20, pad)#find the stable point of the frequency
+	magStable =  stablePoint(conshmag, 0, 40, 10, pad)	#find the stable point of the ampliitude
 
-	pad = 100
-	
-	
-	freqStable = stablePoint(conshfreq, 0, 100, 20, pad)		#find the stable point of the frequency
-	magStable =  stablePoint(conshmag, 0, 40, 10, pad)		#find the stable point of the magnitude
-	print freqStable, magStable
-	stable = max(freqStable, magStable)				#find the stable point of both frequency and magnitude
-	print conshfreq.shape[0]
-	if stable == conshfreq.shape[0]+pad:
-		stable = min(freqStable, magStable)
-	print stable 
-	
-	adjustMagnitude(conshmag, vowelhmag, stable, vowelPadF)
+	stable = max(freqStable, magStable)					#take the maximum for stable point
+	if stable == conshfreq.shape[0]+pad:				#However, the highest never detects one
+		stable = min(freqStable, magStable)				#use the lower value
+
+	adjustMagnitude(conshmag, vowelhmag, stable, vowelPadF)#adjusts the magnitudes of each harmonic
+														#to match with the vowel's harmonics
 
 	consFreqF0 = infoAt(conshfreq, stable)				#find the f0 frequency of the consonant at the stable point
 	vowelFreqF0 = infoAt(vowelhfreq, 1.0*framesPerSecond(vowelFS, samplesPerFrame(vowelX, vowelhfreq)))
-	print "consFreq:",consFreqF0
-	print "vowelFreq:",vowelFreqF0
+														#find the f0 frequency of the vowel at the stable point
 	pR = pitchRatio(consFreqF0, vowelFreqF0)			#find the pitch ration between the frequencies
-	#print pR
-	#print soundLength(consX, consFS), numFrames(conshfreq)
-	initialNumFrames = numFrames(conshfreq)
-	if pR != 1:
-		consX = retune(consFS, consX, 1.0/pR)				#stretch or shrink the consonant as necessary
-	
-	conshfreq, conshmag, conshphase, consxr = fourierResidual(consX, consFS)#re-harmonic-ize the consonant
-	afterRetuneNumFrames = numFrames(conshfreq)
+	initialNumFrames = numFrames(conshfreq)				#number of frames in the consonant sound
+
+	if pR != 1:											#stretch or shrink the consonant as necessary
+		consX = retune(consFS, consX, 1.0/pR)			#to match its pitch to the vowel's
+
+	conshfreq, conshmag, conshphase, consxr = fourierResidual(consX, consFS, minf0 = f0min, maxf0 = f0max)
+														#re-harmonic-ize the consonant
+	afterRetuneNumFrames = numFrames(conshfreq)			#the duration in Frames of the consonant after the stretch
+
 	stable = int(stable*(float(afterRetuneNumFrames)/initialNumFrames))#calculate where the stable frame is after the retune
-	#print stable, newStable
 	consFreqF0 = infoAt(conshfreq, stable)				#find the new frequency at the new stable point
-	#print soundLength(consX, consFS), numFrames(conshfreq)
-	#print pitchRatio(consFreqF0, vowelFreqF0)
-	print stable
-	print "fps",framesPerSecond2(soundLength(consX, consFS), numFrames(conshfreq))
 
 	padAmount = 1000 - (stable*samplesPerFrame(consX, conshfreq)/consFS)*1000
 														#set pad amount to line up the consonant \/ \/ \/ \/
-	consX, consFS = zeroPad(consX, consFS, padAmount)			#add enough samples to put the cut point at 1 second into consonant
-	#print padAmount* 
-	#print phaseAt(conshphase, stable)					#this works because conssphase has not been changed to reflect the pad amount yet
-	#print phaseAt()
-
-	conshfreq, conshmag, conshphase, consxr = fourierResidual(consX, consFS)
-														#construct harmonic arrays from paddedconsonant sound
+	consX, consFS = zeroPad(consX, consFS, padAmount)	#add enough samples to put the cut point at 1 second into consonant
+	conshfreq, conshmag, conshphase, consxr = fourierResidual(consX, consFS, minf0 = f0min, maxf0 = f0max)
+														#create harmonic arrays from the padded consonant
 	stable += padAmount/1000*framesPerSecond2(soundLength(consX, consFS), numFrames(conshfreq))
-	print stable
-	consPhase = infoAt(conshphase, 1.0*framesPerSecond(consFS, samplesPerFrame(consX, conshfreq)))
-	vowelPhase = infoAt(vowelhphase, 1.0*framesPerSecond(vowelFS, samplesPerFrame(vowelX, vowelhfreq)))
-	
+														#add the correct amount of frames to the stable cutpoint value
+	crossFadePitch(conshfreq, vowelhfreq, int(stable), cfLF)
+														#crossfade-tune the pitches of the harmonics to make the transition smoother
+	adjustPhase(H, consFS, conshfreq, conshphase, int(cfLF), int(stable), vowelhphase)
+														#adjust the phases of the harmonics to match with the vowel
+	consX, consXH = makeSound(conshfreq, conshmag, conshphase, consxr, Ns, H, consFS)
+														#write each harmonic (using SMS tools) to a wavy array
 	consFreqF0 = infoAt(conshfreq, 1.0*framesPerSecond(consFS, samplesPerFrame(consX, conshfreq)))
-	sf.write("output_sounds/Cons.wav", consX, consFS)
-	simpleXFade(consX, vowelX, consFS, stable, 10000, conshfreq)
-	#print f0phaseShiftAmount(consPhase, vowelPhase, consFS, consFreqF0)
-	print "elapsed time: {}".format(time.time()-startTime)
+														#f0 of consonant
+	newX = simpleXFade(consX, vowelX, consFS, stable, cfL, conshfreq)
+														#xfade the two using simple cross fade
+	writeSound("output_sounds/xFade.wav", newX, consFS)
+
+
+def main():
+	splice("../Morgan_44.1/Mod/Mod_20.wav", "../Morgan_44.1/Ah Main/Ah Main_20.wav", 1000, 25, 700, 200, 300, Ns=512, H=128)
 if __name__ == '__main__':
 	main()
