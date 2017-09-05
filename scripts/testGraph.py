bf52a6c014aff9564031f70391b0abc6ed09ccb0
alcofrisbas <bgreene231@gmail.com>
2017-09-05 15:00:04 -0700

    added a graph example

---
+++ scripts/testGraph.py
@@ -0,0 +1,8 @@
+import soundOps
+
+def main():
+	soundOps.getInfo("../Morgan_44.1/Zahz/Zahz_02.wav", 60, 100)
+	#soundOps.graphError("../Morgan_44.1/Zahz/Zahz_02.wav", 60, 100)
+
+if __name__ == '__main__':
+	main()
