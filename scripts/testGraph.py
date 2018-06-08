6acf554507d8c16db2769845e2103974884dee39
alcofrisbas <bgreene231@gmail.com>
2018-06-08 11:07:23 -0700

    asdf

--- scripts/testGraph.py
+++
@@ -1,8 +0,0 @@
-import soundOps
-
-def main():
-	soundOps.getInfo("../Morgan_44.1/Zahz/Zahz_02.wav", 60, 100)
-	#soundOps.graphError("../Morgan_44.1/Zahz/Zahz_02.wav", 60, 100)
-
-if __name__ == '__main__':
-	main()
