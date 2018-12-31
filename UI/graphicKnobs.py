750a8ae7bf559551fe36ed559c2e32b158b519b8
Mike's MacBook <MikesMacBook@Mikes-Air.local>
2018-12-30 20:48:22 -0800

    starting new canvas

--- UI/graphicKnobs.py
+++ UI/graphicKnobs.py
@@ -1,6 +1,12 @@
 from Tkinter import *
 import ttk
 from settings import EditField
+import math
+
+def _create_circle(self, x, y, r, **kwargs):
+    return self.create_oval(x-r, y-r, x+r, y+r, **kwargs)
+Canvas.create_circle = _create_circle
+
 class Graphic(ttk.LabelFrame):
 	def __init__(self, master, length,size,name,l=100, r=100, maxRange=400):
 		ttk.LabelFrame.__init__(self, master,text=name)
@@ -105,9 +111,98 @@ class Graphic(ttk.LabelFrame):
 		self.yL = l
 		self.yR = r
 
+
+class ClickableGraphicKnob(ttk.LabelFrame):
+	def __init__(self, master, length,size,name,l=100, r=100, maxRange=400):
+		ttk.LabelFrame.__init__(self, master,text=name)
+		self.maxRange = maxRange
+		self.master = master
+		self.size = size
+		self.length=length
+		# list of tuples; (x, y)
+		self.pts = []
+
+
+		self.adding = False
+		self.removing = False
+
+
+		self.topBox = ttk.Frame(self)
+		self.topBox.grid(row = 0, column = 0)
+
+		self.ctx = Canvas(self.topBox, width=self.size, height=self.size)
+		self.ctx.grid(row=0, column=0)
+
+		self.ctx.bind("<Button 1>", self.canvas_onClick1)
+
+		self.buttonVBox = ttk.Frame(self.topBox)
+		self.buttonVBox.grid(row = 0, column = 1,sticky=N+W)
+
+		self.edit = ttk.Button(self.buttonVBox, text = "edit", width=0,
+			command = self.set_focus)
+		self.edit.grid(row = 0 , column = 0,sticky=W )
+
+		self.plus = ttk.Button(self.buttonVBox, text = "+", width=0,
+			command = self.add_pt)
+		self.plus.grid(row = 1 , column = 0,sticky=W )
+
+		self.minus = ttk.Button(self.buttonVBox, text = "-", width=0,
+			command = self.rem_pt)
+		self.minus.grid(row = 2 , column = 0,sticky=W )
+
+
+
+	def set_focus(self):
+		self.ctx.focus_set()
+
+	def canvas_onClick1(self, event):
+		#print(event.x, event.y)
+		closest = self.find_closest_point((event.x, event.y))
+		if self.pts:
+			item = self.ctx.find_withtag(str(closest))[0]
+			self.highlight_pt(item)
+		
+		elif self.adding:
+			self.pts.append((event.x, event.y))
+			self.draw_pts()
+			self.adding = False
+		
+		elif self.removing:
+			closest = self.find_closest_point((event.x, event.y))
+			self.pts.remove(closest)
+			self.draw_pts()
+			self.removing = False
+
+
+	def find_closest_point(self, pt):
+		d = 2*self.size
+		p = (0,0)
+		for pt1 in self.pts:
+			dist = math.sqrt((pt[0]-pt1[0])**2+(pt[1]-pt1[1])**2)
+			if dist < d:
+				p = pt1
+		return p
+	
+	def add_pt(self):
+		self.adding = True
+		print("adding")
+
+	def rem_pt(self):
+		self.removing = True
+		print("removing")
+	
+	def draw_pts(self):
+		self.ctx.delete("pt")
+		print(self.pts)
+		for pt in self.pts:
+			self.ctx.create_circle(pt[0], pt[1], 3, tags=("pt", str(pt)))
+
+	def highlight_pt(self, ):
+		self.ctx.itemconfig(i, outline='red')
+
 if __name__ == '__main__':
 	root = Tk()
-	g = Graphic(root, 10, 100, "Test")
+	g = ClickableGraphicKnob(root, 10, 100, "Test")
 	g.grid()
 	root.mainloop()
 
